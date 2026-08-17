1ce796faa63450b45762e174535e4ad0e7914b7c
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-17 19:12:47 -0300

    feat: melhor envio services, post para /shipment/calculate pegar  informações do frete

---
+++ src/services/external/melhor-envio-service.ts
@@ -0,0 +1,35 @@
+import {
+  MelhorEnvioRequest,
+  MelhorEnvioResponse,
+} from "../../interfaces/melhor-envio.ts";
+
+const MELHOR_ENVIO_TOKEN = process.env.MELHOR_ENVIO_TOKEN;
+const MELHOR_ENVIO_URL = process.env.MELHOR_ENVIO_URL;
+
+export class MelhorEnvioService {
+  private static getHeaders() {
+    return {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${MELHOR_ENVIO_TOKEN}`,
+    };
+  }
+
+  public static async calcFrete(
+    data: MelhorEnvioRequest,
+  ): Promise<MelhorEnvioResponse[]> {
+    const response = await fetch(
+      `${MELHOR_ENVIO_URL}/api/v2/me/shipment/calculate`,
+      {
+        method: "POST",
+        headers: this.getHeaders(),
+        body: JSON.stringify(data),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error calculating shipping: ${response.statusText}`);
+    }
+
+    return response.json() as Promise<MelhorEnvioResponse[]>;
+  }
+}
