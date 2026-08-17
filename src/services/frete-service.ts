725591e1b973a20895d1813e94416ed04e50bb33
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-17 19:13:57 -0300

    feat: integração do melhor envio service com frete service

--- src/services/frete-service.ts
+++ src/services/frete-service.ts
@@ -1,17 +1,53 @@
 import { CalcFreteRequest, CalcFreteResponse } from "../interfaces/frete.ts";
+import { MelhorEnvioService } from "./external/melhor-envio-service.ts";
+import {
+  MelhorEnvioRequest,
+  MelhorEnvioResponse,
+} from "../interfaces/melhor-envio.ts";
 
 export class FreteService {
   async calcFrete(data: CalcFreteRequest): Promise<CalcFreteResponse> {
-    const { cep_origem, cep_destino, peso, comprimento, altura, largura } =
-      data;
+    const {
+      cep_origem,
+      cep_destino,
+      peso,
+      comprimento,
+      altura,
+      largura,
+      valor_seguro,
+      quantidade,
+    } = data;
 
-    // Not Implemented
-    const valor = 1;
-    const prazoEntrega = 1;
+    const melhorEnvioRequest: MelhorEnvioRequest = {
+      from: {
+        postal_code: cep_origem,
+      },
+      to: {
+        postal_code: cep_destino,
+      },
+      products: [
+        {
+          id: "1",
+          width: largura,
+          height: altura,
+          length: comprimento,
+          weight: peso,
+          insurance_value: valor_seguro,
+          quantity: quantidade,
+        },
+      ],
+    };
+
+    const melhorEnvioResponse: MelhorEnvioResponse[] =
+      await MelhorEnvioService.calcFrete(melhorEnvioRequest);
+
+    if (!melhorEnvioResponse[0]) {
+      throw new Error("No shipping options available");
+    }
 
     return {
-      valor,
-      prazoEntrega,
+      valor: parseFloat(melhorEnvioResponse[0].price),
+      prazoEntrega: melhorEnvioResponse[0].delivery_time,
     };
   }
 }
