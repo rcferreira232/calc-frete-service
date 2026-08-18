57dab3498de905fda63188690c4bf8718112dea7
Kleber <kleber.pandino@gmail.com>
2026-08-17 22:11:21 -0300

    Create request script

---
+++ req.sh
@@ -0,0 +1,1 @@
+curl -Ss --json '{"cep_origem": "96020360", "cep_destino": "01018020", "peso": 1, "comprimento": 11, "altura": 17, "largura": 11, "valor_seguro": 10, "quantidade": 1}' localhost:3333/frete | jq .
