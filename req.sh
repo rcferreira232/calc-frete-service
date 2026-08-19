3d42b78504543f510c9d1423cb30c3f9a50caf5d
Kleber <kleber.pandino@gmail.com>
2026-08-19 12:39:43 -0300

    Format req script

--- req.sh
+++ req.sh
@@ -1,1 +1,11 @@
-curl -Ss --json '{"cep_origem": "96020360", "cep_destino": "01018020", "peso": 1, "comprimento": 11, "altura": 17, "largura": 11, "valor_seguro": 10, "quantidade": 1}' localhost:3333/frete | jq .
+curl -Ss --json \
+'{
+    "cep_origem": "96020360",
+    "cep_destino": "01018020",
+    "peso": 10,
+    "comprimento": 11,
+    "altura": 17,
+    "largura": 11, 
+    "valor_seguro": 10,
+    "quantidade": 1
+}' localhost:3333/frete | jq .
